0da28e379c22ab33e912a3c987ba532d6e36d4b5
Craig Newcomb <CraigNewcomb@Craigs-MacBook-Pro.local>
2016-01-13 18:51:30 -0600

    I added HideShopping

--- HideShopping/ViewController.swift
+++ HideShopping/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var walmart: UIImageView!
+
+    @IBOutlet weak var wholeFood: UIImageView!
+    
+    @IBOutlet weak var walmartButton: UIButton!
+    
+    @IBOutlet weak var wholeFoodButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+  
+    @IBAction func hideWalmart(sender: AnyObject) {
+        walmart.hidden = true
+        walmartButton.hidden = true
+    }
 
+    @IBAction func hideWholeFood(sender: AnyObject) {
+        wholeFood.hidden = true
+        wholeFoodButton.hidden = true
+    }
 }
 
