471d47762f8bae12f33ac485de24eb7320ad7c3b
Craig Newcomb <CraigNewcomb@craigs-macbook-pro.local>
2016-01-13 23:34:09 -0600

    added HEB button/icon that can also be hidden. Added a scene that lets you unhide everything and start over.

--- HideShopping/ViewController.swift
+++ HideShopping/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageButton: UIButton!
+    @IBOutlet weak var heb: UIImageView!
+
+    @IBOutlet weak var hebButton: UIButton!
+    
     @IBOutlet weak var walmart: UIImageView!
 
     @IBOutlet weak var wholeFood: UIImageView!
@@ -28,15 +33,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func unhideButton(sender: AnyObject) {
+        walmart.hidden = false
+        walmartButton.hidden = false
+        wholeFood.hidden = false
+        wholeFoodButton.hidden = false
+        heb.hidden = false
+        hebButton.hidden = false
+        messageButton.hidden = true
+        
+    }
   
     @IBAction func hideWalmart(sender: AnyObject) {
         walmart.hidden = true
         walmartButton.hidden = true
+        checkHidden()
     }
 
     @IBAction func hideWholeFood(sender: AnyObject) {
         wholeFood.hidden = true
         wholeFoodButton.hidden = true
+        checkHidden()
     }
+    @IBAction func hideHEB(sender: AnyObject) {
+        heb.hidden = true
+        hebButton.hidden = true
+        checkHidden()
+    }
+    
+ 
+
+    
+    func checkHidden() {
+        if walmart.hidden == true {
+            if heb.hidden == true {
+                if wholeFood.hidden == true{
+                    messageButton.hidden = false
+                } else{}
+            } else{}
+        } else{}
+    }
+    
+    
+
 }
 
